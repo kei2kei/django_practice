ffea3f4ddaccbf11eab4d028ab1a91d793e3955c
kei2kei <jianye.hongfu@gmail.com>
2026-04-05 10:40:31 +0900

    メール送信用URL追加

--- matching_app/urls.py
+++ matching_app/urls.py
@@ -5,11 +5,14 @@ from matching_app.views.index import index
 from matching_app.views.login import login_view, logout_view
 from matching_app.views.signup import signup
 from matching_app.views.user_profile import user_home
+from matching_app.views.verify import send_new_verification_code, verify_email
 
 urlpatterns = (
   [
     path("", index, name="index"),
     path("signup/", signup, name="signup"),
+    path("signup/verify/<int:id>/", verify_email, name="verify_email"),
+    path("signup/verify/resend/<int:id>/", send_new_verification_code, name="send_new_verification_code"),
     path("login/", login_view, name="login"),
     path("logout/", logout_view, name="logout"),
     path("home/", user_home, name="user_home"),
